082ec83014e3b1deaa3f42d172aa47574fb6591a
dale-smith <dale.smith.workapp01@gmail.com>
2017-08-16 09:24:07 +0200

    Created the Client
    Created the WCF-App Client in a Console Application Format.

---
+++ WCF-App-Service/WCF-App-Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_App_Client
+{
+    class Program
+    {
+        static void Main(string[] args){
+            ServiceReference1.WCFAppServiceClient proxy = new ServiceReference1.WCFAppServiceClient();
+
+        }
+    }
+}
