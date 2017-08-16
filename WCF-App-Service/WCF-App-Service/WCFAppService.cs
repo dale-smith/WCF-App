17b04b3811c3a7a8d8733d56c6b38ab2e9df6fc3
dale-smith <dale.smith.workapp01@gmail.com>
2017-08-16 09:17:45 +0200

    Built the Service
    I renamed and removed certain components. Service has also been built.

---
+++ WCF-App-Service/WCF-App-Service/WCFAppService.cs
@@ -0,0 +1,11 @@
+﻿namespace WCF_App_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class WCFAppService : IWCFAppService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
